00bf1bf82c65dcb0f22a75a523a3e575c06020d9
Rakshith08URK <rajrakshith08@gmail.com>
2024-07-05 14:22:02 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,11 +1,3 @@
-int add(const char* input){
-    // Check if the string is NULL or the first character is '\0'
-   if(isEmptyString(input))
-    {
-   return 0;
-    }
-}
-
 int isEmptyString(const char *str) {
     // Check if the string is NULL or the first character is '\0'
     if(str == NULL || *str == '\0')
@@ -13,5 +5,15 @@ int isEmptyString(const char *str) {
         return 1;
     }
 }
+
+int add(const char* input){
+    // Check if the string is NULL or the first character is '\0'
+   if(isEmptyString(input))
+    {
+   return 0;
+    }
+    return -1;
+}
+
 
 
